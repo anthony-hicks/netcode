3bb3a16b79a716713714bbb1686eee44c612f5ab
Anthony Hicks <anthony-hicks@github.com>
2023-10-11 08:20:51 -0500

    feat: wrap rest of C API
    Since the SDL API don't let you do anything useful with their error codes
    other than print them, the best way to wrap the API is to add ownership
    semantics to pointers and use boolean return types to indicate success.
    
    I considered using std::expected<void, int> to propagate the error code,
    but that would only be useful to defer logging in this case.

--- main.cpp
+++ main.cpp
@@ -1,102 +1,168 @@
 #include <SDL2/SDL.h>
 
-#include <cstdio>
+#include <filesystem>
 #include <format>
 #include <iostream>
 #include <memory>
 
-#define DISABLE_COPY(T) \
-    T(T const&) = delete; \
+#define DISABLE_COPY(T)                                                             \
+    T(T const&) = delete;                                                           \
     T& operator=(T const&) = delete
 
-#define DISABLE_MOVE(T) \
-    T(T&&) = delete; \
+#define DISABLE_MOVE(T)                                                             \
+    T(T&&) = delete;                                                                \
     T& operator=(T&&) = delete
 
 static constexpr int SCREEN_WIDTH = 640;
 static constexpr int SCREEN_HEIGHT = 480;
 
 namespace SDL {
-    namespace detail {
+namespace detail {
 
-        class Library {
-        public:
-            // Initializes the library
-            static bool initialize(uint32_t flags)
-            {
-                if (int err = SDL_Init(flags); err < 0) {
-                    std::clog << std::format("ERROR: SDL_Init ({}): {}", err, SDL_GetError());
-                    return false;
-                }
-
-                static Library library;
-
-                return true;
-            }
-
-            // Shuts down the library
-            ~Library()
-            {
-                // Clean up all initialized subsystems
-                SDL_Quit();
-            }
-
-            DISABLE_COPY(Library);
-            DISABLE_MOVE(Library);
-
-        private:
-            Library() = default;
-        };
-    }
-
-    bool initialize(uint32_t flags)
+class Library {
+public:
+    /// @brief Initializes the library
+    /// @throws std::runtime_error on failure
+    static void initialize(uint32_t flags)
     {
-        return detail::Library::initialize(flags);
+        if (int err = SDL_Init(flags); err < 0) {
+            throw std::runtime_error(
+              std::format("ERROR: SDL_Init ({}): {}", err, SDL_GetError()));
+        }
+
+        static Library library;
     }
 
-    struct WindowDeleter {
-        void operator()(SDL_Window* p) { SDL_DestroyWindow(p); }
-    };
+    /// @brief Shuts down the library
+    ~Library()
+    {
+        // Clean up all initialized subsystems
+        SDL_Quit();
+    }
 
-    using Window = std::unique_ptr<SDL_Window, WindowDeleter>;
+    DISABLE_COPY(Library);
+    DISABLE_MOVE(Library);
+
+private:
+    Library() = default;
+};
+}  // namespace detail
+
+void initialize(uint32_t flags) { return detail::Library::initialize(flags); }
+
+struct WindowDeleter {
+    void operator()(SDL_Window* p) { SDL_DestroyWindow(p); }
+};
+
+using Window = std::unique_ptr<SDL_Window, WindowDeleter>;
+
+struct SurfaceDeleter {
+    void operator()(SDL_Surface* p) { SDL_FreeSurface(p); }
+};
+
+using Surface = std::unique_ptr<SDL_Surface, SurfaceDeleter>;
+
+// No abstraction for SDL_GetWindowSurface because a simple, nullable
+// pointer is accurate (non-owning) and we don't have a need for
+// an actual Window class yet where we could maybe implement a member
+// function: window.surface() -> SDL_Surface*
+//
+// CCG recommends assuming every raw pointer can be null.
+SDL_Surface* GetWindowSurface(SDL_Window* window)
+{
+    // Returns NULL on failure
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+    if (surface == nullptr) {
+        std::clog << std::format("ERROR: SDL_GetWindowSurface (NULL): {}\n",
+                                 SDL_GetError());
+    }
+
+    return surface;
 }
+
+Surface LoadBMP(std::filesystem::path const& path)
+{
+    // Returns NULL on failure
+    std::clog << path.string() << '\n';
+    Surface surface(SDL_LoadBMP(path.string().c_str()));
+
+    if (!surface) {
+        std::clog << std::format("ERROR: SDL_LoadBMP (NULL): {}\n", SDL_GetError());
+    }
+
+    return surface;
+}
+
+// TODO: Doc design of naming with lib UpperCase
+// TODO: Doc design choice:
+// Since SDL doesn't give any API for using the error code,
+// propagating it is useless. We only need it to determine success. In
+// many cases, we should terminate on failure, but sometimes we may
+// be able to recover. So a bool return makes sense.
+bool UpdateWindowSurface(SDL_Window* window)
+{
+    if (int err = SDL_UpdateWindowSurface(window); err < 0) {
+        std::clog << std::format(
+          "ERROR: SDL_UpdateWindowSurface ({}): {}", err, SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool BlitSurface(SDL_Surface* src,
+                 const SDL_Rect* srcrect,
+                 SDL_Surface* dest,
+                 SDL_Rect* destrect)
+{
+    if (int err = SDL_BlitSurface(src, srcrect, dest, destrect); err < 0) {
+        std::clog << std::format("ERROR: SDL_BlitSurface (-1): {}", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+}  // namespace SDL
 
 int main(int argc, char* argv[])
 {
-
     // TODO: .clang-format / .clang-tidy
 
-    if (!SDL::initialize(SDL_INIT_VIDEO)) {
-        return 1;
-    }
+    SDL::initialize(SDL_INIT_VIDEO);
 
     // Create window
     SDL::Window window(SDL_CreateWindow("Title",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN
-    ));
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        SCREEN_WIDTH,
+                                        SCREEN_HEIGHT,
+                                        SDL_WINDOW_SHOWN));
 
-    if (window == nullptr) {
-        printf("Failed to create window: %s\n", SDL_GetError());
+    if (!window) {
         return 1;
     }
 
     // Surface contained by the window
-    SDL_Surface* screen_surface = SDL_GetWindowSurface(window.get());
-    SDL_Surface* bmp_surface = SDL_LoadBMP("hello_world.bmp");
-    if (bmp_surface == nullptr) {
-        printf("Unable to load bmp: %s\n", SDL_GetError());
+    SDL_Surface* screen_surface = SDL::GetWindowSurface(window.get());
+    if (!screen_surface) {
+        return 1;
+    }
+
+    SDL::Surface bmp_surface = SDL::LoadBMP("helloworld.bmp");
+    if (!bmp_surface) {
         return 1;
     }
 
     // Apply the image
-    SDL_BlitSurface(bmp_surface, nullptr, screen_surface, nullptr);
+    if (!SDL::BlitSurface(bmp_surface.get(), nullptr, screen_surface, nullptr)) {
+        return 1;
+    }
 
-    // Update surface
-    SDL_UpdateWindowSurface(window.get());
+    if (!SDL::UpdateWindowSurface(window.get())) {
+        return 1;
+    }
 
     // Hack to get window to stay up
     SDL_Event event;
@@ -109,12 +175,6 @@ int main(int argc, char* argv[])
             }
         }
     }
-
-    SDL_FreeSurface(bmp_surface);
-    bmp_surface = nullptr;
-
-    SDL_DestroyWindow(window.get());
-    SDL_Quit();
 
     return 0;
 }
